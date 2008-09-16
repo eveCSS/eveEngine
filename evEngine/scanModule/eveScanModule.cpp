8bb3aa4a3bd1d1470b6fd849378b1099e06e8318
eden <devnull@localhost>
2008-09-16 13:30:12 +0200

    test

---
+++ evEngine/scanModule/eveScanModule.cpp
@@ -0,0 +1,18 @@
+/*
+ * eveScanModule.cpp
+ *
+ *  Created on: 16.09.2008
+ *      Author: eden
+ */
+
+#include "eveScanModule.h"
+
+eveScanModule::eveScanModule() {
+	// TODO Auto-generated constructor stub
+
+
+}
+
+eveScanModule::~eveScanModule() {
+	// TODO Auto-generated destructor stub
+}
---
+++ evEngine/scanModule/eveScanModule.h
@@ -0,0 +1,17 @@
+/*
+ * eveScanModule.h
+ *
+ *  Created on: 16.09.2008
+ *      Author: eden
+ */
+
+#ifndef EVESCANMODULE_H_
+#define EVESCANMODULE_H_
+
+class eveScanModule {
+public:
+	eveScanModule();
+	virtual ~eveScanModule();
+};
+
+#endif /* EVESCANMODULE_H_ */
