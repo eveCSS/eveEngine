9ad53ed254d0708bce4abc35191da6b2ab558c55
eden <devnull@localhost>
2012-06-18 16:02:04 +0200

    fixed functions which are included in a motor, but not in an axis (trigger unit). A trigger in a motor is called only once now, even if several axes of this motor are used. Completed garbage collection

---
+++ evEngine/scanModule/eveSMMotor.cpp
@@ -0,0 +1,54 @@
+/*
+ * eveSMMotor.cpp
+ *
+ *  Created on: 18.06.2012
+ *      Author: eden
+ */
+
+#include "eveSMMotor.h"
+#include "eveCaTransport.h"
+
+eveSMMotor::eveSMMotor(eveScanModule* scanmodule, eveMotorDefinition* definition) : eveSMBaseDevice(scanmodule) {
+
+	triggerTrans = NULL;
+	unitTrans = NULL;
+	name = definition->getName();
+	scanModule = scanmodule;
+
+	if ((definition->getTrigCmd() != NULL) && (definition->getTrigCmd()->getTrans()!= NULL)){
+		if (definition->getTrigCmd()->getTrans()->getTransType() == eveTRANS_CA){
+			triggerTrans = new eveCaTransport(this, xmlId, name, (eveTransportDef*)definition->getTrigCmd()->getTrans());
+			triggerValue.setType(definition->getTrigCmd()->getValueType());
+			triggerValue.setValue(definition->getTrigCmd()->getValueString());
+		}
+	}
+
+	if (definition->getUnitCmd() != NULL){
+		if (definition->getUnitCmd()->getTrans()== NULL){
+			unitString = definition->getUnitCmd()->getValueString();
+		}
+		else if (definition->getUnitCmd()->getTrans()->getTransType() == eveTRANS_CA){
+			unitTrans = new eveCaTransport(this, xmlId, name, (eveTransportDef*)definition->getUnitCmd()->getTrans());
+		}
+	}
+}
+
+eveSMMotor::~eveSMMotor() {
+	// TODO Auto-generated destructor stub
+}
+
+/**
+ * \brief send an error message to display
+ * @param severity
+ * @param errorType
+ * @param message
+ */
+void eveSMMotor::sendError(int severity, int errorType,  QString message){
+
+	scanModule->sendError(severity, EVEMESSAGEFACILITY_SMDEVICE,
+							errorType,  QString("Motor %1: %2").arg(name).arg(message));
+}
+
+void eveSMMotor::sendError(int severity, int facility, int errorType, QString message){
+	scanModule->sendError(severity, facility, errorType, message);
+}
---
+++ evEngine/scanModule/eveSMMotor.h
@@ -0,0 +1,36 @@
+/*
+ * eveSMMotor.h
+ *
+ *  Created on: 18.06.2012
+ *      Author: eden
+ */
+
+#ifndef EVESMMOTOR_H_
+#define EVESMMOTOR_H_
+
+#include "eveSMBaseDevice.h"
+#include "eveBaseTransport.h"
+#include "eveDevice.h"
+#include "eveVariant.h"
+#include "eveScanModule.h"
+
+class eveSMMotor: public eveSMBaseDevice {
+public:
+	eveSMMotor(eveScanModule*, eveMotorDefinition*);
+	virtual ~eveSMMotor();
+	eveBaseTransport* getTrigTrans(){return triggerTrans;};
+	eveBaseTransport* getUnitTrans(){return unitTrans;};
+	eveVariant getTrigValue(){return triggerValue;};
+	QString getUnitString(){return unitString;};
+	void sendError(int, int, int, QString);
+
+private:
+	void sendError(int, int, QString);
+	eveScanModule* scanModule;
+	eveBaseTransport* triggerTrans;
+	eveBaseTransport* unitTrans;
+	QString unitString;
+	eveVariant triggerValue;
+};
+
+#endif /* EVESMMOTOR_H_ */
