e98d492bc1eb42dc0f5369611badbde06d52e81b
eden <Jens.Eden@ptb.de>
2013-12-20 10:47:41 +0100

    add Copyright statement and license

--- evEngine/main.cpp
+++ evEngine/main.cpp
@@ -1,3 +1,22 @@
+/*
+evEngine - the engine part of eveCSS (http://evecss.github.io)
+
+Copyright (C) 2013  Jens Eden, PTB Germany
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include <stdio.h>
 
@@ -117,10 +136,6 @@ int main(int argc, char *argv[])
 			}
 			if (!eveRoot.isEmpty()) continue;
 		}
-//		else if ( argument.startsWith("-i") ){
-//			interfaces = argument.remove(0,2).trimmed();
-//			if (interfaces.size() > 0) continue;
-//		}
 
 		printf ("\"%s\"?\n", argv[i]);
 	    printf ("usage: %s [-d<number> => debuglevel, -m<number> => messagelevel, -f<xml-File>, -g => Gui on (default off), -n => no network, -p<port>, -e<root directory>\n",argv[0]);
